c18439479396d4e44d678c21213d6d40fa0eba08
NANikolaev <91592401+NANikolaev@users.noreply.github.com>
2022-09-22 01:42:57 +0300

    change mongo locall to atlas

--- server/src/configurations/mongo-db.js
+++ server/src/configurations/mongo-db.js
@@ -1,5 +1,12 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
+const password='sonic_tool'
+const db='BookMarket'
 
-module.exports=()=>{
-    mongoose.connect('mongodb://localhost:27017/BookMarket')
+module.exports = () => {
+    mongoose.connect(`mongodb+srv://tester:${password}@cluster0.mwgyziz.mongodb.net/${db}?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    )
 }
