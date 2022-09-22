4795a1b7475c8e24428ba526490ae5c3926e1e7d
NANikolaev <91592401+NANikolaev@users.noreply.github.com>
2022-09-22 18:34:02 +0300

    change connection strings

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:5000' ; 
+const url = 'https://book--market.herokuapp.com' ; 
 
 let genres=[
     {value:"crime",name:"Crime"},
--- server/src/configurations/express.js
+++ server/src/configurations/express.js
@@ -11,7 +11,5 @@ module.exports=(server)=>{
         methods:'*',
         credentials:true,
         allowedHeaders:"*",
-        
-
     }))
 }
